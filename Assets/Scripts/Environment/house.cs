aecaf07a25ea1c516115abab0402203ec3400644
shiningarc <983399909@qq.com>
2022-12-10 19:40:22 +0800

    修正

--- Assets/Scripts/Environment/house.cs
+++ Assets/Scripts/Environment/house.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class house : MonoBehaviour
 {
+    [SerializeField]
     private MeshRenderer meshren;
     // Start is called before the first frame update
     void Start()
