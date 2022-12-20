7376f04816aa516c7555f6e489be6d7b54d74dda
不笑 <east_laugh@qq.com>
2022-12-20 22:00:38 +0800

    优化NewAreaManager （还没做完）

--- Assets/Scripts/Attribute/ReadOnlyAttribute.cs
+++ Assets/Scripts/Attribute/ReadOnlyAttribute.cs
@@ -5,8 +5,8 @@ using UnityEditor;
 //https://blog.csdn.net/weixin_43737152/article/details/122124474
 public class ReadOnlyAttribute : PropertyAttribute
 {
-    [ReadOnly]
-    int a=3;
+    //[ReadOnly]
+    //int a=3;
 }
 
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
